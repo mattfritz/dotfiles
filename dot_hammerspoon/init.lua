e53515da322760fc5bce9dea05e2a115c4efd97d
Matt Fritz <mfritzer@gmail.com>
2022-03-13 12:26:35 -0600

    Add foreground toggle for Obsidian as well

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -1,8 +1,13 @@
-hs.hotkey.bind({"cmd"}, "escape", function()
-  local alacritty = hs.application.find('alacritty')
-  if alacritty:isFrontmost() then
-    alacritty:hide()
-  else
-    hs.application.launchOrFocus("/Applications/Alacritty.app")
-  end
-end)
+local function bind_toggle(modifiers, key, app_name)
+  hs.hotkey.bind(modifiers, key, function()
+    local app = hs.application.find(app_name)
+    if app:isFrontmost() then
+      app:hide()
+    else
+      hs.application.launchOrFocus('/Applications/'..app_name..'.app')
+    end
+  end)
+end
+
+bind_toggle({'cmd'}, 'escape', 'Alacritty')
+bind_toggle({'cmd', 'shift'}, 'escape', 'Obsidian')
